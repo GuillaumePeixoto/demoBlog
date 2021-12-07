ce251556c6706f3ef6c68def618efcbb6574298f
GuillaumePeixoto <guillaume.duarte78@gmail.com>
2021-12-07 16:58:27 +0100

    Ajout categorie et commentaire

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -10,8 +10,10 @@ use Doctrine\ORM\EntityManager;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class BlogController extends AbstractController
 {
@@ -51,7 +53,6 @@ class BlogController extends AbstractController
         // dump() / dd() : outil de débug de symfony
         //dd($repoArticle);
 
-
         // findAll() : méthode issue de la classe ArticleRepository permettant de selectionner l'ensemble de la table SQL et de récuperer un talbeau multi contenant l'ensemble des articles
         $articles = $repoArticle->findAll(); // SELECT * FROM article + FETCH_ALL
         // dump($articles);
@@ -65,7 +66,7 @@ class BlogController extends AbstractController
 
     #[Route('/blog/new', name: 'blog_create')]
     #[Route('/blog/{id}/edit', name: 'blog_edit')]
-    public function blogCreate(Article $article = null, Request $request, EntityManagerInterface $manager): Response
+    public function blogCreate(Article $article = null, Request $request, EntityManagerInterface $manager, SluggerInterface $slugger): Response
     {
         // Si les données dans le tableau ARRAY $_POST sont supérieur à 0 alors on entre dans la condition
         // if($request->request->count() > 0)
@@ -86,6 +87,12 @@ class BlogController extends AbstractController
         //     $manager->flush();
         // }
 
+        // Si la variable est null, cela veut dire que nous sommes sur la route '/blog/new', on entre dans la condition et on crée une nouvelle instance de l'entité Article
+        if($article)
+        {
+            $photoActuelle = $article->getPhoto();
+        }
+        
         if(!$article)
         {
             $article = new Article;            
@@ -97,23 +104,72 @@ class BlogController extends AbstractController
 
         $formArticle = $this->createForm(ArticleType::class, $article);
 
+        // $article->setTitre($_POST['titre'])
+        // $article->setContenu($_POST['contenu'])
+        // handleRequest() permet d'envoyer chaque donnée de $_POST et de les transmettre aux bon setter de l'objet entité $article
         $formArticle->handleRequest($request);
 
         if($formArticle->isSubmitted() && $formArticle->isValid())
         {
             // Le champs date n'existe pas en tant que formulaire
-            $article->setDate(new \DateTime());
+            if(!$article->getId())
+            {
+                $article->setDate(new \DateTime());
+                $txt = "enregistré";
+            }
+            else
+            {
+                $txt = "modifié";
+            }
+            
+            $photo = $formArticle->get('photo')->getData();
+
+            if($photo)
+            {
+                $nomOriginePhoto = pathinfo($photo->getClientOriginalName(), PATHINFO_FILENAME);
+                //dd($nomOriginePhoto);
+
+                // cela est necessaire pour inclure en toute sécurité le nom du fichier dans l'URL
+                $secureNomPhoto = $slugger->slug($nomOriginePhoto);
+
+                $nouveauNomFichier = $secureNomPhoto.' - '.uniqid().'.'.$photo->guessExtension();
+                // dd($nouveauNomFichier);
+                try
+                {
+                    $photo->move(
+                        $this->getParameter('photo_directory'),
+                        $nouveauNomFichier
+                    );
+                }
+                catch(FileException $e)
+                {
+
+                }
+
+                $article->setPhoto($nouveauNomFichier);
+
+            }
+            else
+            {
+                $article->setPhoto($photoActuelle);
+            }
 
             //dd($article);
+            $this->addFlash('success', "L'article a été $txt avec succès !");
 
             $manager->persist($article);
 
             $manager->flush();
+
+            // Une fois l'insertion/modification terminé, on redirige l'internaute vers l'article
+            return $this->redirectToRoute('blog_show',['id' => $article->getId()]);
         }
 
         return $this->render('blog/blog_create.html.twig',[
-            'formArticle' => $formArticle->createView() // on transmet le formulaire au tempalte afin de pouvoir l'afficher en twig
+            'formArticle' => $formArticle->createView(), // on transmet le formulaire au tempalte afin de pouvoir l'afficher en twig
             // createView() : Retourne un petit objet qui représente
+            'editMode' => $article->getId(),
+            'photoActuelle' => $article->getPhoto()
         ]);
     }
     
